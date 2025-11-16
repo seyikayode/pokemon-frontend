76ca17b111a6907f7ded7210a7b4d834e3d89749
seyikayode <oluwaseyikayode25@gmail.com>
2025-11-16 14:12:23 +0100

    remove proxy

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-    baseURL: '/api/proxy/api'
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 export interface SimplePokemon {
